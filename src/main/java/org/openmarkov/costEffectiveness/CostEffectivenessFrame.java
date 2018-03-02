9808a3bda3b682d8b52b5556d8bb3a161eb4a765
Jorge Pérez <jperezmartin@dia.uned.es>
2018-03-02 11:40:00 +0100

    Change the call to perform CE analysis with DANs

--- src/main/java/org/openmarkov/costEffectiveness/CostEffectivenessFrame.java
+++ src/main/java/org/openmarkov/costEffectiveness/CostEffectivenessFrame.java
@@ -17,7 +17,6 @@ import org.openmarkov.core.model.network.CEP;
 import org.openmarkov.core.model.network.EvidenceCase;
 import org.openmarkov.core.model.network.Finding;
 import org.openmarkov.core.model.network.ProbNet;
-import org.openmarkov.core.model.network.potential.GTablePotential;
 import org.openmarkov.core.model.network.type.DecisionAnalysisNetworkType;
 import org.openmarkov.core.model.network.type.InfluenceDiagramType;
 import org.openmarkov.core.model.network.type.MIDType;
@@ -27,7 +26,7 @@ import org.openmarkov.gui.dialog.inference.common.ScopeType;
 import org.openmarkov.gui.localize.StringDatabase;
 import org.openmarkov.gui.plugin.ToolPlugin;
 import org.openmarkov.gui.window.MainPanel;
-import org.openmarkov.inference.decompositionIntoSymmetricDANs.DecompositionIntoSymmetricDANsEvaluation;
+import org.openmarkov.inference.decompositionIntoSymmetricDANs.CEADANConditionalSymmetricEvaluation;
 import org.openmarkov.inference.variableElimination.tasks.VECEAnalysis;
 
 import javax.swing.*;
@@ -73,9 +72,9 @@ import javax.swing.*;
 						CEPDialog cepDialog = new CEPDialog(owner, cep, probNet);
 						cepDialog.setVisible(true);
 					} else if (probNet.getNetworkType() instanceof DecisionAnalysisNetworkType) {
-						DecompositionIntoSymmetricDANsEvaluation decompositionAlgorithmArticleCEA = new DecompositionIntoSymmetricDANsEvaluation(
-								probNet, preResolutionEvidence);
-						CEP cep = (CEP) ((GTablePotential) decompositionAlgorithmArticleCEA.getUtility()).elementTable
+                        CEAnalysis decompositionAlgorithmArticleCEA = new CEADANConditionalSymmetricEvaluation(
+                                probNet, null, preResolutionEvidence);
+                        CEP cep = (CEP) decompositionAlgorithmArticleCEA.getUtility().elementTable
 								.get(0);
 						CEPDialog cepDialog = new CEPDialog(owner, cep, probNet);
 						cepDialog.setVisible(true);
