dac44b3112b331a5d0de3df92f33b0f0c20926d5
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-07-12 12:41:34 +0200

    [Refactor] Replaced the use of Nls with raw StringDatabase.

--- src/main/java/org/openmarkov/costEffectiveness/CostEffectivenessPlugin.java
+++ src/main/java/org/openmarkov/costEffectiveness/CostEffectivenessPlugin.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.openmarkov.core.exception.OpenMarkovException;
 import org.openmarkov.core.localize.LocalizedException;
-import org.openmarkov.gui.localize.Nls;
+import org.openmarkov.core.localize.StringDatabase;
 import org.openmarkov.gui.toolplugin.ToolPlugin;
 import org.openmarkov.gui.window.MainPanel;
 
@@ -13,7 +13,7 @@ import javax.swing.*;
 public final class CostEffectivenessPlugin implements ToolPlugin {
     
     @Override public @NotNull String menuOptionText() {
-        return Nls.Menus.Tools.CostEffectiveness.Label.stringify();
+        return StringDatabase.getUniqueInstance().getString("Menus", "Tools.CostEffectiveness.Label");
     }
     
     @Override public void showDialog(@Nullable JFrame parent) {
