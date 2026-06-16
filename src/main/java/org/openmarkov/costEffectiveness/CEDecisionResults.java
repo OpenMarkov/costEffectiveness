b212f3425c9146e694423235bf2cffe29c1520e6
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2026-06-16 15:40:11 +0200

    [Bugfix] While on inference mode, it is possible to select states again. [Bugfix] Now nodes with post/pre-resolution evidence have their backgrounds properly showing. [UI] Utility node's border is lighter. Event node is now lighter. Initial and terminal Event nodes have now the same color. [Bugfix] System theme (which is unsupported) can be used again. [Bugfix] Senstivity Analysis button works again. This is the bug 1 of issue 624. [UI] Senstivity Analysis's plot now draw an axis where X=0 and another where Y=0, both are slim black lines. This is the bug 2 of issue 624. [UI] Senstivity Analysis's WTP slope line is now red and thick, instead of black and thin. This is the bug 3 of issue 624. [UI] Senstivity Analysis's Display has been renamed to "Values" (Therefore eliminating the "values" title). States are now shown as "*decision variable name* = *state*" instead of "state *state*". This is the bug 4 of issue 624. [Bugfix] Senstivity Analysis no longer suffers a stutter when changing a value. This is bug 6 of issue 624. [Bugfix] In Sensitivity Analysis it is no longer posible to set WTP below 1. This is bug 7 of issue 624. [UI] Senstivity Analysis's WTP selector is now a NumericSpinner instead of a TextField. This is bug 8 of issue 624. [UI] Minor changes of colors of the Sensitivity Analysis's UI: The plot no longer has a border, the origin no longer appears in the legend, and it is now dashed and has no tip mark, the grid has a very subtle gray color for background and a gray color for the axis of the grid.

--- src/main/java/org/openmarkov/costEffectiveness/CEDecisionResults.java
+++ src/main/java/org/openmarkov/costEffectiveness/CEDecisionResults.java
@@ -829,7 +829,7 @@ public class CEDecisionResults extends JDialog {
 		// Create the line data, renderer, and axis
 		XYDataset collection2 = getLineFrontierInterventionsData();
 		XYItemRenderer renderer2 = new XYLineAndShapeRenderer(true, false);   // Lines only
-        renderer2.setSeriesPaint(0, GUIColors.CostEffectiveness.SERIES_COLOR.getColor());
+        renderer2.setSeriesPaint(0, GUIColors.CostEffectiveness.WTP_SLOPE.getColor());
 		BasicStroke stroke = new BasicStroke();
 		renderer2.setSeriesStroke(0,
 				new BasicStroke(2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1.0f, new float[] { 6.0f, 6.0f },
